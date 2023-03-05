bfd9e4f1ae72dd02717d6a929feef8413b94d9c7
Daniel Pimenov <daniel.pimenov@dxc.com>
2023-03-05 11:13:10 +0100

    working setup for dev profile with h2 db and prod profile with mySql via docker compose

--- src/main/java/com/example/urlshortener/audit/event/UrlEventEntry.java
+++ src/main/java/com/example/urlshortener/audit/event/UrlEventEntry.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Table(name = "URL_EVENTS")
 public class UrlEventEntry {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @ManyToOne
     private ShortenedUrl url;
